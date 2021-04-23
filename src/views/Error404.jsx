f49ad37fb0302cf635b5fba4d6918426c9c865bf
Poccardl <loic.poccard@gmail.com>
2021-04-23 21:27:37 +0200

    add views -> Home.jsx, Login.jsx, Profile.jsx, Error404.jsx

---
+++ src/views/Error404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class Error404 extends React.Component {
+
+    render() {
+        return (
+            <>
+            ERROR - 404
+            </>
+        )
+    }
+}
--- src/views/Routes.jsx
+++ src/views/Routes.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import { Home } from './Home.jsx'
+import { Login } from './Login.jsx'
+import { Profile } from './Profile.jsx'
+import { Error404 } from './Error404.jsx'
+
 export class Routes extends React.Component {
 
     render() {
@@ -8,8 +13,10 @@ export class Routes extends React.Component {
             <>
             <Router>
                 <Switch>
-                    {/* <Route path="/" exact component={Home}></Route>
-                    <Route path="/" component={Error404}></Route> */}
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/login" exact component={Login}></Route>
+                    <Route path="/profile" exact component={Profile}></Route>
+                    <Route path="/" component={Error404}></Route>
                 </Switch>
             </Router>
             </>
