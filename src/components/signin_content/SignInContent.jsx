ecf14bd9150c03d0249b02123f9c424ff80a42bd
Poccardl <loic.poccard@gmail.com>
2021-05-02 23:40:11 +0200

    take inputs of SignIn form

--- src/components/signin_content/SignInContent.jsx
+++ src/components/signin_content/SignInContent.jsx
@@ -4,6 +4,23 @@ import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 export class SignInContent extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            remember: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        const name = e.target.name
+        const type = e.target.type
+        const value = type === 'checkbox' ? e.target.checked : e.target.value
+        this.setState({[name]: value})
+    }
+
     render() {
         return (
             <>
@@ -13,15 +30,15 @@ export class SignInContent extends React.Component {
                 <form>
                     <div className="input-wrapper">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username"/>
+                        <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange}/>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="text" id="password"/>
+                        <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleChange}/>
                     </div>
                     <div className="input-remember">
-                        <input type="checkbox" id="remember-me"/>
-                        <label htmlFor="remember-me">Remember me</label>
+                        <input type="checkbox" id="remember" name="remember" checked={this.state.remember} onChange={this.handleChange}/>
+                        <label htmlFor="remember" >Remember me</label>
                     </div>
                     <a href="..." className="sign-in-button">Sign In</a>
                 </form>
